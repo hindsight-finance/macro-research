5dd4274019eeb157737c1bc4a1e982d11a28d1c8
Thomas Quant <dave.r.0283@gmail.com>
2026-03-09 17:20:23 +0000

    test: cover macro fvg success context figures

--- test/test_macro_fvg_study.py
+++ test/test_macro_fvg_study.py
@@ -1837,3 +1837,5 @@ def test_run_macro_fvg_study_writes_parquet_and_figures(tmp_path):
     assert (figures_dir / "mfe_mae_pct_by_alignment_bucket.png").exists()
     assert (figures_dir / "mfe_pct_by_minute_block.png").exists()
     assert (figures_dir / "mfe_pct_by_gap_bucket.png").exists()
+    assert (figures_dir / "successful_fvg_mae_by_alignment_bucket.png").exists()
+    assert (figures_dir / "successful_fvg_mae_by_stacked_flag.png").exists()
