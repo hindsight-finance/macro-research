a953cbbde17f2661e4978289234414b4cc24b21e
Thomas Quant <dave.r.0283@gmail.com>
2026-04-12 17:55:55 +0100

    test: scaffold trend modeling target package

---
+++ features/trend/modeling/__init__.py
@@ -0,0 +1,3 @@
+from .target import build_descriptive_target
+
+__all__ = ["build_descriptive_target"]
---
+++ features/trend/modeling/test/test_target.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from features.trend.modeling.target import build_descriptive_target
+
+
+def test_build_descriptive_target_returns_bounded_components():
+    result = build_descriptive_target(
+        open_=np.array([100.0, 101.0, 102.0]),
+        high=np.array([101.0, 102.0, 103.0]),
+        low=np.array([99.5, 100.5, 101.5]),
+        close=np.array([100.5, 101.5, 102.5]),
+    )
+
+    assert 0.0 <= result["descriptive_target"] <= 1.0
+    assert set(result) == {
+        "target_strength",
+        "target_consistency",
+        "target_smoothness",
+        "target_retention",
+        "descriptive_target",
+        "target_status",
+    }
